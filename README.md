05bf1c3dee512b020759cd3c6621c32263f26e9a
fanosta <marcel@nageler.org>
2015-09-25 19:07:22 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# config-files
+## disclaimer
+The configuration is to my liking, it may not apply to your preferences.
+This software is provided as is, I am not responsible for any damage done to your system.
+
+## usage
+install [powerline patched fonts](https://github.com/powerline/fonts) onto your system. (Or change the theme in zshrc)
+run ./init.sh to setup a new system
---
+++ init.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if (! which sudo > /dev/null) then
+    >&2 echo install sudo before using this script
+    exit 1
+fi
+
+#update package lists
+#sudo apt-get update
+#install packages
+sudo apt-get install -y zsh tmux vim git mosh software-properties-common python-software-properties
+
+#install oh my zsh
+sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+#get the absolute current path (.)
+WD=$(readlink -f .)
+
+rm ~/.zshrc
+ln -s $WD/zshrc ~/.zshrc
+
+rm ~/.tmux.conf
+ln -s $WD/tmux.conf ~/.tmux.conf
