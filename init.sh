a55c40ee8bdc1ec876ddf4d7ac44c7487dcbdfcf
Marcel Nageler <marcel@nageler.org>
2016-02-11 21:08:13 +0100

    add wd, cleanup zshrc

--- init.sh
+++ init.sh
@@ -8,13 +8,14 @@ fi
 #update package lists
 #sudo apt-get update
 #install packages
-sudo apt-get install -y zsh tmux wget vim git mosh dnsutils software-properties-common python-software-properties xclip
+sudo apt-get install -y zsh tmux wget vim git mosh dnsutils software-properties-common python-software-properties xclip sl
 
 #install oh my zsh
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
 #get the absolute current path (.)
 WD=$(readlink -f .)
+echo $WD
 
 rm ~/.zshrc
 ln -s $WD/zshrc ~/.zshrc
