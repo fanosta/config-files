cae096dddf3df337e33bdbba77be3c9490c0a267
fanosta <github@nageler.org>
2016-05-28 19:51:39 +0200

    promt for hostname in init.sh

--- init.sh
+++ init.sh
@@ -5,18 +5,16 @@ if (! which sudo > /dev/null) then
     exit 1
 fi
 
-#update package lists
-#sudo apt-get update
-#install packages
+# update package lists
+sudo apt-get update
+# install packages
 sudo apt-get install -y zsh tmux wget curl vim git mosh dnsutils software-properties-common python-software-properties xclip sl
-
-#install oh my zsh
-sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
 #get the absolute current path (.)
 WD=$(readlink -f .)
 echo $WD
 
+# link config files
 rm ~/.zshrc
 ln -s $WD/zshrc ~/.zshrc
 
@@ -25,3 +23,13 @@ ln -s $WD/tmux.conf $HOME/.tmux.conf
 ln -s $WD/warprc $HOME/.warprc
 ln -s $WD/vimrc $HOME/.vimrc
 sudo ln -s  $WD/tmx /bin/tmx
+
+# install pathogen (vim plugin manager)
+mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+# install oh my zsh
+# sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+chsh -s $(which zsh)
+
+$WD/change-hostname.sh
